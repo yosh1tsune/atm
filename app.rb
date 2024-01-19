567a3a2ac629901d59aae18c046b5bdcc94a143c
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-19 19:04:17 -0300

    create lib.rb and remove requires from app.rb

--- app.rb
+++ app.rb
@@ -1,16 +1,4 @@
-require_relative 'models/recharge'
-require_relative 'models/withdrawal'
-require_relative 'models/atm'
-require_relative 'models/error'
-
-require_relative 'controllers/recharges_controller'
-require_relative 'controllers/withdrawals_controller'
-
-require_relative 'services/recharge_atm_service'
-require_relative 'services/withdrawal_service'
-
-require 'date_core'
-require 'json'
+require 'lib'
 
 input = nil
 
---
+++ lib.rb
@@ -0,0 +1,14 @@
+require_relative 'models/recharge'
+require_relative 'models/withdrawal'
+require_relative 'models/atm'
+require_relative 'models/error'
+
+require_relative 'controllers/recharges_controller'
+require_relative 'controllers/withdrawals_controller'
+
+require_relative 'services/recharge_atm_service'
+require_relative 'services/withdrawal_service'
+require_relative 'services/select_notes_service'
+
+require 'date_core'
+require 'json'
