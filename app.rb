22d395485638f0738f3a0b27c830c2644e50eefe
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-19 15:20:30 -0300

    update app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require_relative 'models/recharge'
 require_relative 'models/withdrawal'
 require_relative 'models/atm'
+require_relative 'models/error'
 
 require_relative 'controllers/recharges_controller'
+require_relative 'controllers/withdrawals_controller'
 
 require_relative 'services/recharge_atm_service'
+require_relative 'services/withdrawal_service'
+
+require 'date_core'
 require 'json'
 
 input = nil
@@ -18,13 +23,14 @@ while input != 'exit' do
     if json.has_key?(:caixa)
       RechargesController.recharge(json)
     elsif json.has_key?(:saque)
-      WithdrawalsController.withdraw
+      WithdrawalsController.withdraw(json)
     end
   rescue => e
-    puts 'Operação inválida.'
   end
 end
 
 { "caixa":{ "caixaDisponivel":true, "notas":{ "notasDez":100, "notasVinte":50, "notasCinquenta":10, "notasCem":30 } } }
 
 {"saque":{"valor":80,"horario":"2019-02-13T11:01:01.000Z"}}
+
+{"saque":{"valor":80,"horario":"2019-02-13T11:25:01.000Z"}}
