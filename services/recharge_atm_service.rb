6ea91dead023bdde9a99f56097a97bed47df9465
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-19 15:19:53 -0300

    update recharge service

--- services/recharge_atm_service.rb
+++ services/recharge_atm_service.rb
@@ -8,20 +8,16 @@ class RechargeATMService
   def execute
     atm = ATM.first
 
-    raise StandardError if atm.caixaDisponivel
+    raise ATMUnderUseError if atm.caixaDisponivel
 
     atm = atm.save(json)
 
     response(atm)
   rescue => e
-    response(atm, 'caixa-em-uso')
+    response(atm, e.message)
   end
 
   private
-
-  def add_bank_notes
-
-  end
 
   def response(atm, error = '')
     puts "\n\n"
