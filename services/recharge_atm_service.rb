96ac52d2d182f032845fd9a7268f5cf44e700953
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-20 12:01:08 -0300

    refactor rcharge atm service

--- services/recharge_atm_service.rb
+++ services/recharge_atm_service.rb
@@ -6,29 +6,38 @@ class RechargeATMService
   end
 
   def execute
-    atm = ATM.first
-
     raise ATMUnderUseError if atm.caixaDisponivel
 
-    atm = atm.save(json)
+    atm.save(json)
 
-    response(atm)
-  rescue => e
-    response(atm, e.message)
+    response(atm.reload)
+  rescue ATMUnderUseError => e
+    response(atm.reload, e.message)
   end
 
   private
 
+  def atm
+    @atm ||= if ATM.first
+               ATM.first
+             else
+               ATM.create({
+                 caixaDisponivel: false,
+                 notas: { notasDez: 0, notasVinte: 0, notasCinquenta: 0, notasCem: 0 }
+               })
+             end
+  end
+
   def response(atm, error = '')
     puts "\n\n"
-    pp(
+    puts(
       {
         'caixa': {
           'caixaDisponivel': atm.caixaDisponivel,
           'notas': atm.notas,
           'erros': [error]
         }
-      }
+      }.to_json
     )
   end
 end
