bb26f9e940e82af23c05024c08cd6e1d3622727d
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-20 12:00:27 -0300

    include exception class

--- models/error.rb
+++ models/error.rb
@@ -2,7 +2,13 @@ class Error < StandardError
 end
 
 class ATMUnderUseError < Error
-  def initialize(msg='caixa-indisponivel')
+  def initialize(msg='caixa-em-uso')
+    super
+  end
+end
+
+class InexistentATMError < Error
+  def initialize(msg='caixa-inexistente')
     super
   end
 end
