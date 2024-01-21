26c01bd57c99113151015186934962b53e8c093d
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-21 15:47:44 -0300

    remove useless comments

--- app.rb
+++ app.rb
@@ -25,21 +25,3 @@ while json != 'exit'
     WithdrawalsController.new(json).withdrawal
   end
 end
-
-# while input != 'exit'
-#   begin
-#     puts "\n\nEntrada:"
-#     input = gets.chomp
-
-#     json = JSON.parse(input, symbolize_names: true)
-
-#     if json.key?(:caixa)
-#       RechargesController.new(json).recharge
-#     elsif json.key?(:saque)
-#       WithdrawalsController.new(json).withdrawal
-#     end
-#   rescue StandardError => e
-#     puts e.message
-#     puts e.backtrace
-#   end
-# end
