c53ba3a6bf55bdb5d57e82dd0e42112a6b9936d6
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-21 15:46:03 -0300

    update app.rb to handle multiple lines insert

--- app.rb
+++ app.rb
@@ -3,21 +3,43 @@
 require_relative 'lib'
 
 input = nil
+json = ''
+while json != 'exit'
+  puts "\n\nEntrada:"
 
-while input != 'exit'
-  begin
-    puts "\n\nEntrada:"
-    input = gets.chomp
+  json = ''
+  loop do
+    input = $stdin.sysread(1)
+    json += input if input != "\n"
+  rescue EOFError
+    break
+  end
 
-    json = JSON.parse(input, symbolize_names: true)
+  break if json == 'exit'
 
-    if json.key?(:caixa)
-      RechargesController.new(json).recharge
-    elsif json.key?(:saque)
-      WithdrawalsController.new(json).withdrawal
-    end
-  rescue StandardError => e
-    puts e.message
-    puts e.backtrace
+  json = JSON.parse(json, symbolize_names: true)
+
+  if json.key?(:caixa)
+    RechargesController.new(json).recharge
+  elsif json.key?(:saque)
+    WithdrawalsController.new(json).withdrawal
   end
 end
+
+# while input != 'exit'
+#   begin
+#     puts "\n\nEntrada:"
+#     input = gets.chomp
+
+#     json = JSON.parse(input, symbolize_names: true)
+
+#     if json.key?(:caixa)
+#       RechargesController.new(json).recharge
+#     elsif json.key?(:saque)
+#       WithdrawalsController.new(json).withdrawal
+#     end
+#   rescue StandardError => e
+#     puts e.message
+#     puts e.backtrace
+#   end
+# end
