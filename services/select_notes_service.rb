f2531b8d5f7d8dbf38d3b747a4141b8a16889db4
Bruno Silva <brunorsilva_@hotmail.com>
2024-01-21 02:01:54 -0300

    rubocop fixes

--- services/select_notes_service.rb
+++ services/select_notes_service.rb
@@ -3,6 +3,13 @@
 class SelectNotesService
   attr_accessor :withdrawal, :atm
 
+  POSSIBLE_NOTES = [
+    { name: :notasCem, value: 100 },
+    { name: :notasCinquenta, value: 50 },
+    { name: :notasVinte, value: 20 },
+    { name: :notasDez, value: 10 }
+  ]
+
   def initialize(withdrawal:)
     @withdrawal = withdrawal
     @atm = ATM.first
@@ -23,28 +30,28 @@ class SelectNotesService
   def process_withdrawal
     value = withdrawal.valor
     while value != 0
-      if value >= 100 && atm.notas[:notasCem].positive?
-        value = select_notes(value, 100, :notasCem)
-      elsif value >= 50 && atm.notas[:notasCinquenta].positive?
-        value = select_notes(value, 50, :notasCinquenta)
-      elsif value >= 20 && atm.notas[:notasVinte].positive?
-        value = select_notes(value, 20, :notasVinte)
-      elsif value >= 10 && atm.notas[:notasDez].positive?
-        value = select_notes(value, 10, :notasDez)
+      POSSIBLE_NOTES.map do |note|
+        if value >= note[:value] && atm.notas[note[:name]].positive?
+          value = select_notes(value, note[:value], note[:name])
+        end
       end
     end
     atm.notas
   end
 
-  def select_notes(withdrawal_value, note_value, value_name)
+  def select_notes(withdrawal_value, note_value, note_name)
     quantity_needed = (withdrawal_value / note_value)
-    if atm.notas[value_name] < quantity_needed
-      withdrawal_value -= (atm.notas[value_name] * note_value)
-      atm.notas[value_name] = 0
+    if atm.notas[note_name] < quantity_needed
+      withdrawal_value -= value_in_notes(atm.notas[note_name], note_value)
+      atm.notas[note_name] = 0
     else
-      withdrawal_value -= (quantity_needed * note_value)
-      atm.notas[value_name] = atm.notas[value_name] - quantity_needed
+      withdrawal_value -= value_in_notes(quantity_needed, note_value)
+      atm.notas[note_name] -= quantity_needed
     end
     withdrawal_value
+  end
+
+  def value_in_notes(quantity, nominal_value)
+    quantity * nominal_value
   end
 end
